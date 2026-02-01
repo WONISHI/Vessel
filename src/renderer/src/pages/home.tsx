e5acc657945f70bca51560f5b5a1e0eefebceb23
weizhifeng <weizhifeng@weizhifengdeMacBook-Pro.local>
2026-02-01 11:38:29 +0800

    补全首页导入按钮

--- src/renderer/src/pages/home.tsx
+++ src/renderer/src/pages/home.tsx
@@ -1,5 +1,7 @@
 import Logo from '@/assets/vessel.png'
 import { Card, CardContent } from '@/components/ui/card'
+import { ButtonGroup } from "@/components/ui/button-group"
+import { Button } from '@renderer/components/ui/button'
 
 export default function HomePage(): React.JSX.Element {
   return (
@@ -7,6 +9,10 @@ export default function HomePage(): React.JSX.Element {
       <Card className="m-auto">
         <CardContent>
           <img src={Logo} className="w-[156px] h-[156px]" />
+          <ButtonGroup>
+            <Button>导入文件</Button>
+            <Button>进入笔记</Button>
+          </ButtonGroup>
         </CardContent>
       </Card>
     </>
