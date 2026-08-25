d4762f00dbc3e0b99a7933a033cad492af0d2582
weizhifeng <18797942292@139.com>
2026-08-25 14:50:53 +0800

    feat: 新增字体

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,5 +1,17 @@
 import "./assets/main.css"
 
+// 引入所有字重
+import "@fontsource/plus-jakarta-sans"
+
+// 或按需引入特定字重（推荐，减小体积）
+import "@fontsource/plus-jakarta-sans/400.css" // Regular
+import "@fontsource/plus-jakarta-sans/500.css" // Medium
+import "@fontsource/plus-jakarta-sans/600.css" // SemiBold
+import "@fontsource/plus-jakarta-sans/700.css" // Bold
+
+// 斜体
+import "@fontsource/plus-jakarta-sans/400-italic.css"
+
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App"
